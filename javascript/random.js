92742eb9e5777a5d5793a0a84edf5e87ab90aad8
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-05-06 00:27:06 -0700

    added more code examples to random.js file

--- javascript/random.js
+++ javascript/random.js
@@ -1,3 +1,8 @@
 var random = Math.random();
 random=random*5;
 console.log(random)
+
+//love calculator 
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(random)+1;
+console.log(loveScore)
