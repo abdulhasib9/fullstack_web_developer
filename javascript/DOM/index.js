20daf25a10ba41f70c7e2fa6c7e94e66f123c3b7
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-05-08 23:43:08 -0700

    added more html and js code examples

--- javascript/DOM/index.js
+++ javascript/DOM/index.js
@@ -1,3 +1,5 @@
 //alert("working")
 document.querySelector("h1").innerHTML="Hello from DOM!"
 //document.getElementsByClassName("title").innerHTML="Hello from DOM!"
+
+document.getElementsByTagName("li")[1].style="color:green";
