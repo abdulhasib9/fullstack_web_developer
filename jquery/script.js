d57a78a16354140f2465a725b7f71fe649cd0774
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-14 13:54:20 -0700

    added more jquery code examples and adding dynamic elements using jquery

--- jquery/script.js
+++ jquery/script.js
@@ -1,1 +1,34 @@
 $("h1").css("color","blue")
+
+ $(document).ready(()=>{
+    $("h1").addClass("big-heading")
+    $("h1").text("Change text")
+ })
+
+ //change the attributes 
+ $("a").attr("href","https://www.yahoo.com")
+ $("a").addClass('big-heading')
+
+ $("a").click(function(){
+    $("a").addClass("brown")
+ })
+ 
+ //catching keypress events
+$("#numbers").keypress(function (e) { 
+    console.log(e.key)
+    let keys =[]
+    keys.push(e.key)
+    console.log(keys)
+ });
+
+ //adding elements using jquery
+$("h1").prepend("<span>dynamic text</span>");
+$("h1").before("<button>Click</button>")
+
+//adding animation
+$("button").on("click",function(){
+    $("input").hide();
+})
+
+
+
