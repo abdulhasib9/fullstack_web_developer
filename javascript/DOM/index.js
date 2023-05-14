676a7c42fb3502e00e93d2aebf5f7ef035d24423
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-14 13:07:20 -0700

    added jquery and starter files

--- javascript/DOM/index.js
+++ javascript/DOM/index.js
@@ -3,3 +3,4 @@ document.querySelector("h1").innerHTML="Hello from DOM!"
 //document.getElementsByClassName("title").innerHTML="Hello from DOM!"
 
 document.getElementsByTagName("li")[1].style="color:green";
+console.log("working")
