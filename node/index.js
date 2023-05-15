3538b433c69bc9a391a3fe7ea3e6cd5ac790cb6f
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-14 17:24:25 -0700

    added more node js and npm package manager examples

---
+++ node/index.js
@@ -0,0 +1,9 @@
+let superheroes = require("superheroes")
+let superVillains = require(("supervillains"))
+let my_superHeroName=superheroes.random()
+console.log(my_superHeroName)
+
+
+let mySuperVillains=superVillains.random()
+console.log(mySuperVillains
+    )
--- node/script.js
+++ node/script.js
@@ -1,1 +1,3 @@
 const fs= require("fs")
+
+fs.copyFileSync("file.txt","file2.txt")
