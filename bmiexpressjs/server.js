6afab2231c022be38a80b2d1c680c8fe346609e6
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-15 22:27:01 -0700

    added index.html file and some js code examples for processing the form data

--- bmiexpressjs/server.js
+++ bmiexpressjs/server.js
@@ -5,7 +5,16 @@ const app = express()
 //using body parser 
 app.use(bodyParser.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
-    res.send("home page")
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/",(req,res)=>{
+    let weight = parseFloat(req.body.weight)
+    let height = parseFloat(req.body.height)
+    let result = weight /(height* height)
+   // console.log(result)
+    res.send("Your BMI is : "+result)
+
 })
 
 app.listen(3000,()=>{
