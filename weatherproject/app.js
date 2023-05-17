07e728185eb74575493d3ce83fe5b9eabc9e290a
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-16 23:52:13 -0700

    added final functionalities for fetching live data from the api

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -1,14 +1,34 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const https = require("https")
 const app = express()
 
-
+app.use(bodyParser.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
 
     res.sendFile(__dirname+"/index.html")
 })
 
+app.post("/",(req,res)=>{
+    const city = req.body.city
+    const apiKey="appid=98d3f68623faa82cfbedcbc444c32abc"
+    const url = "https://api.openweathermap.org/data/2.5/weather?"+apiKey+"&q="+city;
+    https.get(url,(response)=>{
+        response.on("data",(data)=>{
+            let readableData = JSON.parse(data)
+            let temp = readableData.main.temp
+
+            res.write("<h1>the weather in "+city+" is "+temp+"</h1>")
+            console.log(readableData)
+        })
+    })
+   
+   // console.log(req.body.city);
+   // res.send("Request received!")
+})
+
+
+//https://api.openweathermap.org/data/2.5/weather?&q=sacramento
 
 // app.get("/",(req,res)=>{
 //     const url ="https://api.openweathermap.org/data/2.5/weather?zip=95823,us&appid=98d3f68623faa82cfbedcbc444c32abc&q=sacramento"
