3954190fb5e3135c898f7571282a5eddb30851fe
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-05-28 22:46:12 -0700

    added views directory and todo.ejs file and implement the ejs templating engine

--- templatingjs/app.js
+++ templatingjs/app.js
@@ -1,6 +1,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
+app.set("view engine", "ejs")
+
+app.get("/",(req,res)=>{
+
+    let currentDate = new Date()
+    let today = currentDate.getDay()
+    let day =""
+    if(today ===4){
+        day ="Horry its Weekend"
+    }else{
+        day="opp's it work day"
+    }
+    res.render("todo",{currentDayType:day})
+
+    //res.sendFile(__dirname+"/index.html")
+})
 
 app.listen(3000,()=>{
     console.log("the express server has been started on port 3000")
