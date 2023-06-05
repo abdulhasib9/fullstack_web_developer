bf7d20a3999dc8d323e237bed48287eb6493d8ad
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-06-04 23:32:53 -0700

    added new form in todo.ejs file and add new route and code logic for todo list items in app.js file

--- templatingjs/app.js
+++ templatingjs/app.js
@@ -2,10 +2,10 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 app.set("view engine", "ejs")
-
+app.use(bodyParser.urlencoded({extended:true}))
+let items =[]
 app.get("/",(req,res)=>{
 
-   
     let today = new Date()
     let options={
         weekday:"long",
@@ -16,9 +16,17 @@ app.get("/",(req,res)=>{
     let day = today.toLocaleDateString("en-US",options)
 
 
-    res.render("todo",{currentDayType:day})
+    res.render("todo",{currentDayType:day,newItem:items})
 
     //res.sendFile(__dirname+"/index.html")
+})
+
+//route for adding todo items
+app.post("/add",(req,res)=>{
+    let  item = req.body.addInput
+    console.log(item)
+    items.push(item)
+    res.redirect("/")
 })
 
 app.listen(3000,()=>{
